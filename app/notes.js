1876c767e4ead8449258febd34d966ab40780eaa
BackRunner <liaoyp126@126.com>
2018-07-03 19:59:16 +0800

    add forever id

--- app/notes.js
+++ app/notes.js
@@ -1,6 +1,32 @@
+//import electron-json-storage
+const storage = require('electron-json-storage');
+
 let textarea = $('#note-text');
 let fs = require('fs');
 let time = require('./tools/time.js');
+
+
+//预设notesid
+let notesid = 0;
+
+//获取notesid的数据
+storage.get('notesid',function(error,data){
+    if (error){
+        notesid = 1;
+        return;
+    }
+    var notesid_json = data;
+    if (notesid_json == null || notesid_json == undefined){
+        notesid = 1;
+        return;
+    }
+    notesid_json = JSON.parse(notesid_json);
+    notesid = notesid_json.id;
+    console.log(notesid);
+    if (notesid == null || notesid == undefined){
+        notesid = 1;
+    }
+});
 
 if (!fs.existsSync('./notes/')){
     fs.mkdir('./notes/');
@@ -19,6 +45,7 @@ textarea.keyup(function (e) {
 //保存note为json
 function saveNote(notetext) {
     var note = {
+        id: notesid,
         time: time.getCurrentTime(),
         rawtime:time.getRawCurrentTime(),
         timezone: time.getTimeZone(),
@@ -32,4 +59,14 @@ function saveNote(notetext) {
             textarea.val('');
         }
     });
+    notesid++;
+    saveNotesId();
+}
+
+//保存ID
+function saveNotesId(){
+    var data = {
+        id:notesid
+    }
+    storage.set('notesid',JSON.stringify(data));
 }
