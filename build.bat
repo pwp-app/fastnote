5b095e295c0f4d6e3c0f8ce4b106b0e2184137af
BackRunner <liaoyp126@126.com>
2019-01-25 18:07:12 +0800

    change to less

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 f:
+cd "F:\Projects\Electron Project\Fastnote\views\less"
+lessc main.default.less main.css
 cd "F:\Projects\Electron Project\Fastnote"
 npm run build
 pause
---
+++ views/less/build.bat
@@ -0,0 +1,1 @@
+lessc main.default.less main.css
