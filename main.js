a4decd112256b7498fd84e6b2fdf52a1f7f44685
BackRunner <liaoyp126@126.com>
2019-03-20 18:26:57 +0800

    update Readme.md

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const {
 
 //global settings
 global.indebug = false; //debug trigger
-global.isOS64 = false; //OS flag
+global.isOS64 = true; //OS flag
 global.firstStart = false; //first start flag
 global.uuid = ""; //uuid storage
 
