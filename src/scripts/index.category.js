01e1c3a7a87d56b952f8c2bf2a52ba02c3696460
BackRunner <liaoyp126@126.com>
2019-04-14 00:33:41 +0800

    添加category的UI逻辑

---
+++ src/scripts/index.category.js
@@ -0,0 +1,13 @@
+var categories = new Array();
+
+//放置新的Category到数组
+function putNewCategory(name){
+    categories[name] = Category(name, 0);
+}
+
+function Category(name, count){
+    this.name = name;
+    this.count = count;
+    return this;
+}
+
