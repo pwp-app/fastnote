ec415ebc2a177741df582649f45046097603bb55
BackRunner <liaoyp126@126.com>
2019-07-17 02:50:48 +0800

    dismissable infobar

--- app/infobar.js
+++ app/infobar.js
@@ -1,29 +1,40 @@
 var infobar_container;
 
 $(document).ready(function(){
-    infobar_container = $(".container-infobar");
+    infobar_container = $("#electron-titlebar");
 });
 
 var infobar_id = 0;
+var infobar_timeout = {};
 
 //显示Infobar
-function displayInfobar(type, text, timeout = 3000) {
+function displayInfobar(type, text, timeout = 3000, dismiss = true) {
     var html = "";
     switch (type) {
         case 'success':
             html += '<div class="infobar infobar-success" id="infobar_' + infobar_id + '"><i class="fa fa-check"></i><span>';
             html += text;
-            html += '</span></div>';
+            html += '</span>';
+            if (dismiss){
+                html += '<i class="fa fa-times infobar-dismiss" onclick="closeInfobar('+infobar_id+');"></i>';
+            }
+            html += '</div>';
             break;
         case 'warning':
             html += '<div class="infobar infobar-warning" id="infobar_' + infobar_id + '"><i class="fa fa-warning"></i><span>';
             html += text;
-            html += '</span></div>';
+            if (dismiss){
+                html += '<i class="fa fa-times infobar-dismiss" onclick="closeInfobar('+infobar_id+');"></i>';
+            }
+            html += '</div>';
             break;
         case 'error':
             html += '<div class="infobar infobar-error" id="infobar_' + infobar_id + '"><i class="fa fa-warning"></i><span>';
             html += text;
-            html += '</span></div>';
+            if (dismiss){
+                html += '<i class="fa fa-times infobar-dismiss" onclick="closeInfobar('+infobar_id+');"></i>';
+            }
+            html += '</div>';
             break;
         case 'update':
             timeout = 0; //禁用自动关闭
@@ -34,7 +45,10 @@ function displayInfobar(type, text, timeout = 3000) {
         default:
             html += '<div class="infobar infobar-info" id="infobar_' + infobar_id + '"><i class="fa fa-info-circle"></i><span>';
             html += text;
-            html += '</span></div>';
+            if (dismiss){
+                html += '<i class="fa fa-times infobar-dismiss" onclick="closeInfobar('+infobar_id+');"></i>';
+            }
+            html += '</div>';
             break;
     }
     infobar_container.append($(html));
@@ -44,7 +58,7 @@ function displayInfobar(type, text, timeout = 3000) {
         //用t中转
         var t = infobar_id;
         $('#infobar_' + t).animateCss('fadeInDown', function () {
-            setTimeout(function () {
+            infobar_timeout.infobar_id = setTimeout(function () {
                 $('#infobar_' + t).animateCss('fadeOutUp', function () {
                     $('#infobar_' + t).remove();
                 });
@@ -58,6 +72,13 @@ function displayInfobar(type, text, timeout = 3000) {
         $('#infobar_update').animateCss('fadeInDown');
     }
     infobar_id++;
+}
+
+function closeInfobar(id){
+    clearTimeout(infobar_timeout.id);
+    $('#infobar_' + id).animateCss('fadeOutUp', function () {
+        $('#infobar_' + id).remove();
+    });
 }
 
 function clearInfoBarContainer() {
