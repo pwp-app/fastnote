d9cc6a8dd91c901fc9a90b004ba6fb90c5647893
BackRunner <liaoyp126@126.com>
2019-07-18 01:25:23 +0800

    add cloud panel

--- app/cloud/config.js
+++ app/cloud/config.js
@@ -1,9 +1,11 @@
 const config = {
     dev: {
+        protocol: 'http',
         host: 'localhost',
         port: 8000
     },
     release: {
+        protocol: 'http',
         host: 'note.pwp.app',
         port: 8000
     }
