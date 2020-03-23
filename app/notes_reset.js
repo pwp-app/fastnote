5f0c56959536ba1c8c4b29fa858ca157789ec9c7
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-03-24 00:48:24 +0800

    refactor: replace some async dialog with sync

--- app/notes_reset.js
+++ app/notes_reset.js
@@ -3,40 +3,39 @@ let fs = require('fs');
 var storagePath = app.getPath('userData');
 
 $('#btn-resetNotes').click(function(){
-    dialog.showMessageBox({
+    let ret = dialog.showMessageBoxSync({
         type:"warning",
         buttons:['取消','确认'],
         defaultId:0,
         title:'确认操作',
         message:'确定要重置便签吗？该操作将不可撤回。',
         detail:'所有已保存的便签、回收站内的便签将全部清除，重置后便签序号从0开始重新计算。'
-    }).then(res => {
-        if (res.response == 1){
-            storage.remove('notesid'+(global.indebug?'_dev':''), function(error){
-                if (error){
-                    console.error(error);
-                    dialog.showMessageBox({
-                        type:"error",
-                        title:'错误',
-                        message:'重置便签时遇到了错误。',
-                        detail:error
-                    });
-                } else {
-                    deleteall(storagePath  + (global.indebug?'/devTemp':'')+ '/notes');
-                    //通知其他窗体重新载入
-                    ipcRenderer.send('reloadMainWindow');
-                    ipcRenderer.send('reloadRecycleWindow');
-                    ipcRenderer.send('closeAllEditWindow');
-                    reloadNotesId();
-                    dialog.showMessageBox({
-                        type:"info",
-                        title:'完成',
-                        message:'便签已完全重置。'
-                    });
-                }
-            });
-        }
     });
+    if (ret == 1) {
+        storage.remove('notesid'+(global.indebug?'_dev':''), function(error){
+            if (error){
+                console.error(error);
+                dialog.showMessageBoxSync({
+                    type:"error",
+                    title:'错误',
+                    message:'重置便签时遇到了错误。',
+                    detail:error
+                });
+            } else {
+                deleteall(storagePath  + (global.indebug?'/devTemp':'')+ '/notes');
+                //通知其他窗体重新载入
+                ipcRenderer.send('reloadMainWindow');
+                ipcRenderer.send('reloadRecycleWindow');
+                ipcRenderer.send('closeAllEditWindow');
+                reloadNotesId();
+                dialog.showMessageBoxSync({
+                    type:"info",
+                    title:'完成',
+                    message:'便签已完全重置。'
+                });
+            }
+        });
+    }
 });
 
 //删除目录、子目录、目录下的所有文件
