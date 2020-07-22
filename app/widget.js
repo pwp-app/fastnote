b92592cedb7f6ff55ef3c4e9af5776c96c12fa5c
BackRunner <dev@backrunner.top>
2020-07-23 01:00:40 +0800

    add: widget spawn calculation

--- app/widget.js
+++ app/widget.js
@@ -1,40 +1,96 @@
 let widgets = [];
 let widgets_noteid = [];
 
-const {
-    BrowserWindow
-} = require('electron');
+const { BrowserWindow } = require('electron');
 
 const electron = require('electron');
 const ipc = require('electron').ipcMain;
 const path = require('path');
 
-let targetY = -1;
+// 获得目标高度的算法
+function merge(intervals) {
+    if (intervals.length == 0) {
+        return intervals;
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    let res = [];
+    res.push(
+        intervals.reduce((acc, cur) => {
+            if (acc[1] >= cur[0]) {
+                if (acc[1] < cur[1]) {
+                    acc[1] = cur[1];
+                }
+                return acc;
+            } else {
+                res.push(acc);
+                return cur;
+            }
+        })
+    );
+    return res;
+}
+
+function getTarget(screenWidth, screenHeight, x_limit) {
+    // 极端情况，堆不下了
+    if (screenWidth - x_limit < 478) {
+        return [0, 18];
+    }
+    let ranges = [];
+    for (let widget of widgets) {
+        const bounds = widget.getBounds();
+        if (bounds.x < screenWidth - x_limit - 478 || bounds.x > screenWidth - x_limit - 18) {
+            continue;
+        }
+        const start = bounds.y;
+        const end = bounds.y + bounds.height;
+        ranges.push([start, end]);
+    }
+    if (ranges.length < 1) {
+        return [x_limit, 18];
+    }
+    // 对ranges进行合并
+    ranges = merge(ranges);
+    // 检查顶部有没有空隙
+    if (ranges[0][0] > 300) {
+        return [x_limit, 18];
+    }
+    // 检查中间有没有空隙
+    for (let i = 0; i < ranges.length - 1; i++) {
+        if (ranges[i+1][0] - ranges[i][1] > 380) {
+            return [x_limit, ranges[i][1] + 8];
+        }
+    }
+    // 检查底部有没有空隙
+    if (ranges[ranges.length - 1][1] < screenHeight - 318) {
+        return [x_limit, ranges[ranges.length - 1][1] + 18];
+    }
+    return getTarget(screenWidth, screenHeight, x_limit + 468);
+}
 
 function createWidget(data) {
-    let screenWidth = electron.screen.getPrimaryDisplay().size.width;
-    if (targetY < 0){
-        targetY = 18;
-    }
+    const screenSize = electron.screen.getPrimaryDisplay().size;
+    const screenWidth = screenSize.width;
+    const screenHeight = screenSize.height;
+
+    const target = getTarget(screenWidth, screenHeight, 0);
+
     const conf = {
-        x: screenWidth - 478,
-        y: targetY,
+        x: screenWidth - target[0] - 478,
+        y: target[1],
         width: 460,
         height: 280,
         minWidth: 320,
         minHeight: 86,
         show: false,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         },
         resizable: true,
         transparent: true,
-        skipTaskbar: true
+        skipTaskbar: true,
     };
-    if (process.platform == 'darwin')
-        conf.titleBarStyle = 'hiddenInset';
-    else
-        conf.frame = false;
+    if (process.platform == 'darwin') conf.titleBarStyle = 'hiddenInset';
+    else conf.frame = false;
 
     let widget = new BrowserWindow(conf);
     widgets.push(widget);
@@ -48,91 +104,42 @@ function createWidget(data) {
     }
     widget.loadFile(viewpath);
 
-    widget.on('close', ()=>{
-        let widget_x = widget.getPosition()[0];
-        let widget_y = widget.getPosition()[1];
-        let screenWidth = electron.screen.getPrimaryDisplay().size.width;
-        if (widget_x == screenWidth - 478){
-            if (widget_y < targetY && widget_y >= 18){
-                targetY = widget_y;
-            }
-        }
-    });
-
     widget.on('closed', () => {
         let index = widgets.indexOf(widget);
         widgets.splice(index, 1);
         widgets_noteid.splice(index, 1);
-        if (widgets.length < 1){
-            targetY = -1;
-        }
     });
 
-    widget.on('ready-to-show', ()=>{
-        ipc.once('widget-window-ready',()=>{
+    widget.on('ready-to-show', () => {
+        ipc.once('widget-window-ready', () => {
             widget.show();
         });
-        ipc.once('widget-heightChange', (sender, height)=>{
+        ipc.once('widget-heightChange', (sender, height) => {
             widget.setSize(widget.getSize()[0], height);
-            targetY = targetY + height + 8;
         });
-        ipc.once('widget-setMaxHeight', (sender, height)=>{
+        ipc.once('widget-setMaxHeight', (sender, height) => {
             widget.setMaximumSize(screenWidth, height);
         });
         widget.webContents.send('init', data.note);
     });
-
-    widget.once('will-move', ()=>{
-        let index = widgets.indexOf(widget);
-        let widget_y = widgets[index].getPosition()[1];
-        if (widget_y < targetY){
-            targetY = widget_y;
-        }
-    });
 }
 
-ipc.on('widget-alwaysontop', (sender, data)=>{
-    let index = widgets_noteid.indexOf(data);
-    if (typeof widgets[index] != 'undefined' || widgets[index] != null){
-        if (widgets[index].isAlwaysOnTop()){
-            widgets[index].setAlwaysOnTop(false);
-            widgets[index].webContents.send('setAlwaysOnTop', false);
-        } else {
-            widgets[index].setAlwaysOnTop(true);
-            widgets[index].webContents.send('setAlwaysOnTop', true);
-        }
-    }
-});
-
-ipc.on('widget-lock', (sender, data)=>{
-    let index = widgets_noteid.indexOf(data);
-    if (typeof widgets[index] != 'undefined' || widgets[index] != null){
-        if (widgets[index].isResizable()){
-            widgets[index].setResizable(false);
-            widgets[index].webContents.send('setLock', true);
-        } else {
-            widgets[index].setResizable(true);
-            widgets[index].webContents.send('setLock', false);
-        }
-    }
-});
-
-ipc.on('reloadAllWidgets', ()=>{
-    for (let i=0; i<widgets.length; i++){
+ipc.on('reloadAllWidgets', () => {
+    for (let i = 0; i < widgets.length; i++) {
         if (typeof widgets[i] != 'undefined' && widgets[i] != null) {
             widgets[i].webContents.send('readyToReload');
         }
     }
 });
 
-ipc.on('widget-reload-ready', (e, data)=>{
+ipc.on('widget-reload-ready', (e, data) => {
     e.sender.reload();
     e.sender.once('did-finish-load', () => {
         e.sender.send('init', data);
     });
 });
 
-ipc.on('closeAllWidgets', ()=>{
+ipc.on('closeAllWidgets', () => {
     for (var i = 0; i < widgets.length; i++) {
         if (typeof widgets[i] != 'undefined' && widgets[i] != null) {
             widgets[i].close();
@@ -143,29 +150,29 @@ ipc.on('closeAllWidgets', ()=>{
 });
 
 var DesktopWidget = {
-    getWindows: ()=>{
+    getWindows: () => {
         return widgets;
     },
-    getNoteIds: ()=>{
+    getNoteIds: () => {
         return widgets_noteid;
     },
-    create: (data)=>{
+    create: (data) => {
         let index = widgets_noteid.indexOf(data.note.id);
-        if (index != -1){
+        if (index != -1) {
             widgets[index].focus();
         } else {
             createWidget(data);
         }
     },
-    updateEditNote: (data)=>{
+    updateEditNote: (data) => {
         let note = data.note;
         let index = widgets_noteid.indexOf(note.id);
-        if (index >= 0){
-            if (typeof widgets[index] != 'undefined' && widgets[index] != null){
+        if (index >= 0) {
+            if (typeof widgets[index] != 'undefined' && widgets[index] != null) {
                 widgets[index].webContents.send('update-edit-note', data.note);
             }
         }
-    }
+    },
 };
 
 module.exports = DesktopWidget;
