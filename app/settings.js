a2f77da22bdab23791fddec87fc9be07a4fe08ff
BackRunner <dev@backrunner.top>
2021-03-11 22:14:51 +0800

    minor: format

--- app/settings.js
+++ app/settings.js
@@ -1,83 +1,78 @@
-
 let win_settings = null;
 
-const {
-    BrowserWindow
-} = require('electron');
+const { BrowserWindow } = require('electron');
 
 const ipc = require('electron').ipcMain;
 const path = require('path');
 
 function createSettingsWindow() {
-    let conf = {
-        width: 680,
-        height: 592,
-        resizable: false,
-        maximazable: false,
-        useContentSize: true,
-        show: false,
-        webPreferences: {
-            enableRemoteModule: true,
-            nodeIntegration: true
-        }
-    };
-    //标题栏的选用
-    if (process.platform == 'darwin')
-        conf.titleBarStyle = 'hiddenInset';
-    else
-        conf.frame = false;
+  let conf = {
+    width: 680,
+    height: 592,
+    resizable: false,
+    maximazable: false,
+    useContentSize: true,
+    show: false,
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true,
+    },
+  };
+  //标题栏的选用
+  if (process.platform == 'darwin') conf.titleBarStyle = 'hiddenInset';
+  else conf.frame = false;
 
-    win_settings = new BrowserWindow(conf);
+  win_settings = new BrowserWindow(conf);
 
-    let viewpath;
-    if (global.hotfix && global.hotfix.state !== 'close') {
-        viewpath = global.hotfix.buildPath('settings.html');
-    } else {
-        viewpath = path.resolve(__dirname, '../public/settings.html');
-    }
-    win_settings.loadFile(viewpath);
+  let viewpath;
+  if (global.hotfix && global.hotfix.state !== 'close') {
+    viewpath = global.hotfix.buildPath('settings.html');
+  } else {
+    viewpath = path.resolve(__dirname, '../public/settings.html');
+  }
+  win_settings.loadFile(viewpath);
 
-    ipc.on('settings-window-heightChange', function(sender, height){
-        win_settings.setContentSize(680, height);
+  ipc.on('settings-window-heightChange', function (sender, height) {
+    win_settings.setContentSize(680, height);
+  });
+
+  win_settings.on('closed', () => {
+    win_settings = null;
+  });
+
+  win_settings.on('ready-to-show', () => {
+    ipc.on('settings-window-ready', () => {
+      win_settings.show();
     });
-
-    win_settings.on('closed', () => {
-        win_settings = null;
-    });
-
-    win_settings.on('ready-to-show', ()=>{
-        ipc.on('settings-window-ready', () => {
-            win_settings.show();
-        });
-    });
+  });
 }
 
-ipc.on('reloadSettingsWindow',function(sender, data){
-    win_settings.reload();
+ipc.on('reloadSettingsWindow', function (sender, data) {
+  win_settings.reload();
 });
 
 const settingsWindow = {
-    show: () => {
-        if (win_settings) {
-            if (win_settings.isMinimized()) {
-                win_settings.restore();
-            }
-            win_settings.focus();
-        } else {
-            createSettingsWindow();
-        }
-    },
-    get: () => {
-        return win_settings;
-    },
-    getFocus: () => {
-        if (win_settings) {
-            if (win_settings.isMinimized()) {
-                win_settings.restore();
-            }
-            win_settings.focus();
-        }
+  show: () => {
+    if (win_settings) {
+      if (win_settings.isMinimized()) {
+        win_settings.restore();
+      }
+      win_settings.focus();
+    } else {
+      createSettingsWindow();
     }
+  },
+  get: () => {
+    return win_settings;
+  },
+  getFocus: () => {
+    if (win_settings) {
+      if (win_settings.isMinimized()) {
+        win_settings.restore();
+      }
+      win_settings.focus();
+    }
+  },
 };
 
 module.exports = settingsWindow;
